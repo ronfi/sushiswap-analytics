b948fc6ba31cdee8381b805cc0e479291dcd0102
terryhash <terryhash@protonmail.com>
2021-08-28 16:20:00 +0800

    export sushi pool

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import {
   Search,
   TokenTable,
 } from "app/components";
-import { Box, Grid, Paper } from "@material-ui/core";
+import { Box, Grid, Paper, Button } from "@material-ui/core";
 import React, { useState } from "react";
 import {
   dayDatasQuery,
@@ -28,6 +28,68 @@ import {
 import Head from "next/head";
 import { ParentSize } from "@visx/responsive";
 import { useQuery } from "@apollo/client";
+
+
+function ExportToFile(pairs, tokens) {
+  // exportObjectAsJson(poolDatas)
+  console.log('ExportToFile --- 1111111');
+
+  const tokenMap = new Map();
+  tokens.forEach(token => {
+    tokenMap.set(token.symbol, token.decimals);
+  });
+
+  const jsonArray = [];
+  pairs.forEach(pair => {
+    const token0 = pair.token0;
+    const token1 = pair.token1;
+    const token0Map = tokenMap.get(token0.symbol);
+    const token1Map = tokenMap.get(token1.symbol);
+
+    if (!token0Map) {
+      console.log('unrecogonized token: ' + token0);
+      return;
+    }
+
+    if (!token1Map) {
+      console.log('unrecogonized token: ' + token1);
+      return;
+    }
+    
+    const token0Update = {
+      address:    token0.id,
+      decimals:   token0Map.decimals,
+      derivedETH: token0.derivedETH,
+      name:       token0.name,
+      symbol:     token0.symbol,
+    };
+    const token1Update = {
+      address:    token1.id,
+      decimals:   token1Map.decimals,
+      derivedETH: token1.derivedETH,
+      name:       token1.name,
+      symbol:     token1.symbol,
+    };
+    const element = {
+      poolAddress: pair.id,
+      token0: token0Update,
+      token1: token1Update,
+      feeTier: 3000,
+    };
+    jsonArray.push(element);
+  });
+
+  const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(jsonArray))
+  const downloadAnchorNode = document.createElement('a')
+  downloadAnchorNode.setAttribute('href', dataStr)
+  downloadAnchorNode.setAttribute('download', 'sushi_pools.json')
+  document.body.appendChild(downloadAnchorNode) // required for firefox
+  downloadAnchorNode.click()
+  downloadAnchorNode.remove()
+
+  console.log('ExportToFile --- 2222222');
+}
+
 
 function IndexPage() {
   const {
@@ -127,6 +189,8 @@ function IndexPage() {
             </ParentSize>
           </Paper>
         </Grid>
+
+        <Button color="primary" onClick={() => ExportToFile(pairs, tokens)}>Export</Button>
 
         <Grid item xs={12}>
           <PairTable title="Top Sushi Liquidity Pairs" pairs={pairs} />
